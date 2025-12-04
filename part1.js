1d5b52f02ea98325e244bd39201b365c910292ee
Zion Danielson <236946026+ZionD01@users.noreply.github.com>
2025-12-04 17:28:04 -0500

    Part 1 Complete

---
+++ part1.js
@@ -0,0 +1,27 @@
+// Part 1: Fizz Buzz
+// Loop through all numbers from 1 to 100.
+// If a number is divisible by 3, log “Fizz.”
+// If a number is divisible by 5, log “Buzz.”
+// If a number is not divisible by either 3 or 5, log the number.
+
+for (let i = 0; i <= 100; i++) {
+
+    if (i % 3 == 0 && i % 5 == 0) {
+
+        console.log(`${i} Fizz Buzz`);
+
+    } else if (i % 3 == 0) {
+
+        console.log(`${i} Fizz`);
+
+    } else if (i % 5 == 0) {
+
+        console.log(`${i} Buzz`);
+
+    } else {
+        
+        console.log(i);
+    }
+}
+
+//Part 2: 
