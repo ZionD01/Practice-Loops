aa903b8f25048d98ad54683c02ddca175034ddb0
Zion Danielson <236946026+ZionD01@users.noreply.github.com>
2025-12-04 22:59:02 -0500

    Began Part 3

---
+++ part3.js
@@ -0,0 +1,48 @@
+// Part 3: Feeling Loopy
+
+let str = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26';
+
+
+// Loop through the characters of a given CSV string.
+
+for (let i = 0; i <= str.length; i++) {
+
+    console.log(i);
+
+}
+
+
+// Store each “cell” of data in a variable.
+
+let cell1 = '';
+let cell2 = '';
+let cell3 = '';
+let cell4 = '';
+let cell5 = '';
+let cell6 = '';
+let cell7 = '';
+let cell8 = '';
+let cell9 = '';
+let cell10 = '';
+let cell11 = '';
+let cell12 = '';
+let cell13 = '';
+let cell14 = '';
+let cell15 = '';
+let cell16 = '';
+let cell17 = '';
+let cell18 = '';
+let cell19 = "";
+let cell20 = '';
+
+
+// When you encounter a comma, move to the next cell.
+
+
+
+
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
+
